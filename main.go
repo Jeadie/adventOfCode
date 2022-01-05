1657d7326f3460262779594a64b052db1e4b97cc
Jack Eadie <jeadi@amazon.com>
2022-01-05 21:01:48 +1000

    BFS of fifteen that will fail at large grids

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 type adventCode struct {
@@ -10,10 +12,10 @@ type adventCode struct {
 
 // To consider: https://felixge.de/2021/12/01/advent-of-go-profiling-2021-day-1-1/
 func main() {
-	//days := [25]adventCode{{One}, {Two}, {Three}, {Four}, {Five}, {Six}, {Seven}, {Eight},
-	//	{Nine}, {Ten}, {Eleven}, {Twelve}, {Thirteen}, {Fourteen}, {Fifteen}, {Sixteen}}
-	//
-	//adventDay, _ := strconv.ParseInt(os.Args[1], 10, 8)
-	//x := days[adventDay-1].fn()
-	fmt.Println(Sixteen())
+	days := [25]adventCode{{One}, {Two}, {Three}, {Four}, {Five}, {Six}, {Seven}, {Eight},
+		{Nine}, {Ten}, {Eleven}, {Twelve}, {Thirteen}, {Fourteen}, {Fifteen}, {Sixteen}}
+
+	adventDay, _ := strconv.ParseInt(os.Args[1], 10, 8)
+	x := days[adventDay-1].fn()
+	fmt.Println(x)
 }
