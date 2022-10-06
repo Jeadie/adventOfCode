c26d59e86e6bd0b5e11385ff1a95f03af2e1ab7d
jeadi <jeadi@amazon.com>
2022-10-06 14:36:50 +1000

    18 puzzle; forgot to upload from macOS

--- eighteen.go
+++ eighteen.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 /**
 --- Day 18: Snailfish ---
 
@@ -166,8 +170,369 @@ The final sum is:
 
 The magnitude of this final sum is 4140. Add up all of the snailfish numbers from the homework assignment in the order
 they appear. What is the magnitude of the final sum?
+
+-- Soln --
+
+    			[[6,[5,[4,[3,2]]]],1]  -->  (Becomes [[6,[5,[7,0]]],3])
+					/         \
+			[6,[5,[4,[3,2]]]]  1
+				/      \
+				6	[5,[4,[3,2]]]
+						/      \
+						5	[4,[3,2]]
+							  /   \
+							  4  [3,2]
+								  / \
+								 3   2
+Considering [[[[[9,8],1],2],3],4] in tree form
+
+			[[[[[9,8],1],2],3],4]
+				/      \
+	[[[[9,8],1],2],3]   4
+		/          \
+	[[[9,8],1],2]   3
+		/      \
+	[[9,8],1]   2
+	/      \
+  [9,8]     1
+   / \
+  9   8
+
+Then we can understand operations of SnailFish arithmetics with tree semantics
+ - "If any pair is nested inside four pairs, the leftmost such pair explodes."
+	--> Perform in-order traversal and find first pair, if present, at depth=4. Add left to the next left at
+		depth=3 (if no right branch, d=2, ...). When pair found, add to most right element. (i.e. perform post order
+		traversal on d=3 (or 2, 1) pair). Similarly, for right, find parent with right node, add to left most child of
+		parent node.
+
+ - "If any regular number is 10 or greater, the leftmost such regular number splits."
+	-->  Perform in-order traversal and find first leaf node with value>=10. If found, convert into pair of
+		[v//2, (v+1)//2]
+
+ - If neither of these traversals provide an action, then it is reduced.
+
+ - To add to Snailfish numbers, construct new tree with summands as leafs: i.e. for  a+b, evaluate the tree
+	a+b
+    / \
+   a   b
+
+  - "The magnitude of a pair is 3 times the magnitude of its left element plus 2 times the magnitude of its right
+	element"
+	--> Perform post order traversal. For a node |v| = 3*|left| + 2*|right|
 */
 
 func Eighteen() interface{} {
+	sfns := GetAllSnailfishNumbers()
+
+	// Reduce all SnailFishNumbers
+	for _, sfn := range sfns {
+		start := sfn.Print()
+		(&sfn).Reduce()
+		fmt.Println(fmt.Sprintf("DONE WITH: %s --> %s", start, (&sfn).Print()))
+		fmt.Println()
+	}
+
+	// Add leading two SnailfishNumbers, then reduce. Combine all SnailfishNumbers like this.
+	initSfn := &sfns[0]
+	for _, sfn := range sfns[1:] {
+		fmt.Println(fmt.Sprintf("Starting. %s + %s", initSfn.Print(), sfn.Print()))
+
+		addSfn := Add(initSfn, &sfn)
+		fmt.Println("Added.", addSfn.Print())
+		fmt.Println("l r .", addSfn.left.Print(), addSfn.right.Print())
+		(&addSfn).Reduce()
+
+		fmt.Println(fmt.Sprintf("Result.    = %s", addSfn.Print()))
+		initSfn = &addSfn
+	}
+
+	// Compute magnitude of final SnailfishNumber
+	return initSfn.Magnitude()
+}
+
+func GetAllSnailfishNumbers() []SnailfishNumber {
+	var sfns []SnailfishNumber
+	s := GetScanner(18)
+	for s.Scan() {
+		sfn := ConstructSnailFishNumber(s.Text(), nil)
+		sfns = append(sfns, sfn)
+	}
+	return sfns
+}
+
+func ConstructSnailFishNumber(s string, parent *SnailfishNumber) SnailfishNumber {
+	var left SnailfishNumber
+	var rightIndex int
+	result := SnailfishNumber{
+		parent: parent,
+	}
+
+	if s[1] >= 48 && s[1] <= 57 {
+		left = SnailfishNumber{
+			v:      string(s[1]),
+			left:   nil,
+			right:  nil,
+			parent: &result,
+		}
+		rightIndex = 3
+
+	} else {
+		left = ConstructSnailFishNumber(s[1:], &result)
+		rightIndex = len(left.v) + 2
+	}
+
+	var right SnailfishNumber
+	if s[rightIndex] >= 48 && s[rightIndex] <= 57 {
+		right = SnailfishNumber{
+			v:      string(s[rightIndex]),
+			left:   nil,
+			right:  nil,
+			parent: &result,
+		}
+	} else {
+		right = ConstructSnailFishNumber(s[rightIndex:], &result)
+	}
+	result.v = fmt.Sprintf("[%s,%s]", left.v, right.v)
+	result.left = &left
+	result.right = &right
+	return result
+
+}
+
+type SnailfishNumber struct {
+	v      string
+	left   *SnailfishNumber
+	right  *SnailfishNumber
+	parent *SnailfishNumber
+}
+
+func (sfn SnailfishNumber) IsLeaf() bool {
+	return (sfn.left == nil) && (sfn.right == nil)
+}
+
+func (sfn *SnailfishNumber) isLeftChild() bool {
+	parent := sfn.parent
+	defer func() {
+		// recover from panic if one occured. Set err to nil otherwise.
+		if recover() != nil {
+			fmt.Println("PANICKED", parent.Print(), sfn.Print())
+			panic("")
+		}
+	}()
+	return *parent.left == *sfn
+
+}
+
+func (sfn *SnailfishNumber) Print() string {
+	if sfn == nil {
+		return ""
+	}
+	if sfn.IsLeaf() {
+		if len(sfn.v) > 1 {
+			return fmt.Sprintf("%d%d", sfn.v[0]-48, sfn.v[1]-48)
+		}
+		return fmt.Sprintf("%d", sfn.v[0]-48)
+	} else {
+		return fmt.Sprintf("[%s,%s]", sfn.left.Print(), sfn.right.Print())
+	}
+}
+
+func (sfn *SnailfishNumber) Reduce() {
+	// Precedence: Explode, Split
+
+	for reductionOccurred := true; reductionOccurred; reductionOccurred = sfn.Explode() || sfn.Split() {
+		//fmt.Println()
+		//fmt.Println("START OF ROUND:", sfn.Print())
+	}
+}
+
+func Add(x *SnailfishNumber, y *SnailfishNumber) SnailfishNumber {
+	root := SnailfishNumber{
+		v:      fmt.Sprintf("[%s,%s]", x.Print(), y.Print()),
+		left:   x,
+		right:  y,
+		parent: nil,
+	}
+	x.parent = &root
+	y.parent = &root
+	return root
+}
+
+func (sfn *SnailfishNumber) Magnitude() int {
+	if sfn.IsLeaf() {
+		return unsafeIntParse(sfn.v)
+	}
+	return sfn.left.Magnitude() + sfn.right.Magnitude()
+}
+
+//- "If any pair is nested inside four pairs, the leftmost such pair explodes."
+//--> Perform in-order traversal and find first pair, if present, at depth=4. Add left to the next left at
+//depth=3 (if no right branch, d=2, ...). When pair found, add to most right element. (i.e. perform post order
+//traversal on d=3 (or 2, 1) pair). Similarly, for right, find parent with right node, add to left most child of
+//parent node.
+
+func (sfn *SnailfishNumber) Explode() bool {
+	//fmt.Println("Exploding....", sfn.Print())
+	explodeSfn := GetSfnToExplode(sfn, 0)
+	if explodeSfn == nil {
+		//fmt.Println("No explosion")
+		return false
+	}
+
+	// Convert leaf value, E.g. "3" -> into uint8 3 before use
+	// TODO: don't respect if explodeSfn.parent.left = explodeSfn
+	//fmt.Println("exploding ")
+	//PrintSnailfishNumber(sfn)
+	//PrintSnailfishNumber(explodeSfn)
+
+	explodeSfn.addToLeftParent(explodeSfn.left.v[0] - 48)
+	explodeSfn.addToRightParent(explodeSfn.right.v[0] - 48)
+
+	// Convert exploded Snailfish Number to a "0".
+	if explodeSfn.isLeftChild() {
+		*(explodeSfn.parent.left) = SnailfishNumber{
+			v:      "0",
+			left:   nil,
+			right:  nil,
+			parent: explodeSfn.parent,
+		}
+	} else {
+		*(explodeSfn.parent.right) = SnailfishNumber{
+			v:      "0",
+			left:   nil,
+			right:  nil,
+			parent: explodeSfn.parent,
+		}
+	}
+
+	return true
+}
+
+func PrintSnailfishNumber(sfn *SnailfishNumber) {
+	if sfn.parent != nil {
+		fmt.Printf("root: %s\n", sfn.parent.Print())
+	}
+	fmt.Printf("node: %s\n", sfn.Print())
+	if sfn.IsLeaf() {
+		return
+	}
+	fmt.Printf("left: %s\n", sfn.left.Print())
+	fmt.Printf("right: %s\n", sfn.right.Print())
+}
+
+func GetSfnToExplode(sfn *SnailfishNumber, depth int) *SnailfishNumber {
+	// Don't explode leaf node (i.e. a literal number), explode its parent (e.g. explode [3,4] not 3)
+	if sfn.IsLeaf() && depth >= 3+1 {
+		return sfn.parent
+	}
+	if sfn.IsLeaf() {
+		return nil
+	}
+
+	l := GetSfnToExplode(sfn.left, depth+1)
+	if l != nil {
+		return l
+	}
+
+	r := GetSfnToExplode(sfn.right, depth+1)
+	if r != nil {
+		return r
+	}
+
 	return nil
 }
+
+func (sfn *SnailfishNumber) Split() bool {
+	//fmt.Println("Splitting....", sfn.Print())
+	splitSfn := GetSfnToSplit(sfn)
+	if splitSfn == nil {
+		//fmt.Println("No split")
+		return false
+	}
+
+	splitV := splitSfn.v[0] - 48
+	splitSfn.left = &SnailfishNumber{
+		v:      string((splitV / 2) + 48),
+		left:   nil,
+		right:  nil,
+		parent: splitSfn,
+	}
+
+	splitSfn.right = &SnailfishNumber{
+		v:      string(((splitV + 1) / 2) + 48),
+		left:   nil,
+		right:  nil,
+		parent: splitSfn,
+	}
+	return true
+}
+
+func GetSfnToSplit(sfn *SnailfishNumber) *SnailfishNumber {
+	if sfn.IsLeaf() && sfn.v[0] > 57 {
+		return sfn
+	}
+	if sfn.IsLeaf() {
+		return nil
+	}
+
+	l := GetSfnToSplit(sfn.left)
+	if l != nil {
+		return l
+	}
+
+	r := GetSfnToSplit(sfn.right)
+	if r != nil {
+		return r
+	}
+
+	return nil
+
+}
+
+func (sfn *SnailfishNumber) addToRightChild(v uint8) {
+	if sfn.IsLeaf() {
+		sfn.v = string(sfn.v[0] + v)
+	} else {
+		sfn.right.addToRightChild(v)
+	}
+}
+
+func (sfn *SnailfishNumber) addToLeftChild(v uint8) {
+	if sfn.IsLeaf() {
+		sfn.v = string(sfn.v[0] + v)
+	} else {
+		sfn.left.addToLeftChild(v)
+	}
+}
+
+func (sfn *SnailfishNumber) addToLeftParent(v uint8) {
+	if sfn.parent != nil && !sfn.isLeftChild() {
+		sfn.parent.left.addToRightChild(v)
+	} else if sfn.parent == nil {
+		return
+	} else {
+		sfn.parent.addToLeftParent(v)
+	}
+}
+
+func (sfn *SnailfishNumber) addToRightParent(v uint8) {
+	if sfn.parent != nil && sfn.isLeftChild() {
+		sfn.parent.right.addToRightParent(v)
+	} else if sfn.parent == nil {
+		return
+	} else {
+		sfn.parent.addToRightParent(v)
+	}
+}
+
+/**
+		[[[10,0],[15,9]],[[8,15],[0,[5,4]]]]
+     		/ 				\
+		[[10,0],[15,9]]    [[8,15],[0,[5,4]]]
+		  /    \				/    \
+		[10,0]  [15, 9]		[8, 15]  [0,[5,4]]
+		 / \      /  \		  /  \      /  \
+		10, 0    15   9      8   15    0   [5,4]
+											/ \
+										   5   4
+*/
